1844f4b08462764e25c0dd8d7f7fe8e5b8a668a5
Preetha1408-T <preethatamilselvan1408@gmail.com>
2026-04-05 14:51:22 +0530

    Update navbar.jsx

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -6,25 +6,81 @@ const Navbar = () => {
   const company = useSelector((state) => state.company);
 
   return (
-    <AppBar position="static" color="primary" sx={{ bgcolor:"blueblack",top:"auto",bottom:0,  width: { xs: 520, sm:"100%", md: "100%" }, }}>
-      <Toolbar sx={{ flexDirection: "column", alignItems: "flex-start", py: 4 }}>
+    <AppBar
+      position="static"
+      color="primary"
+      sx={{
+        bgcolor: "blueblack",
+        top: "auto",
+        bottom: 0,
+        width: { xs: 520, sm: "100%", md: "100%" },
+      }}
+    >
+      <Toolbar
+        sx={{ flexDirection: "column", alignItems: "flex-start", py: 4 }}
+      >
         <Grid container spacing={4}>
           {/* Company Info Column */}
           <Grid item xs={12} sm={4}>
             <Typography variant="h6" color="white" gutterBottom>
               {company?.name}
             </Typography>
+
             <Typography variant="body2" color="grey.300">
-              TEL: {company?.phone}
+              Email:{" "}
+              <Link
+                href={`mailto:${company?.email}`}
+                color="inherit"
+                underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                {company?.email}
+              </Link>
             </Typography>
+
             <Typography variant="body2" color="grey.300">
-              Mobile Phone: {company?.phone}
+              TEL:{" "}
+              <Link
+                href={`tel:${company?.phone}`}
+                color="inherit"
+                underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                {company?.phone}
+              </Link>
+            </Typography>
+
+            <Typography variant="body2" color="grey.300">
+              Mobile Phone:{" "}
+              <Link
+                href={`tel:${company?.phone}`}
+                color="inherit"
+                underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                {company?.phone}
+              </Link>
             </Typography>
             <Typography variant="body2" color="grey.300">
               Contact: Sales Manager
-            </Typography>
-            <Typography variant="body2" color="grey.300">
-              Email: {company?.email}
             </Typography>
           </Grid>
 
@@ -32,25 +88,112 @@ const Navbar = () => {
           <Grid item xs={12} sm={8}>
             <Grid container spacing={2}>
               <Grid item xs={6} sm={3}>
-                <Typography variant="subtitle2" color="grey.400">Company Profile</Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">Overview</Link></Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">History</Link></Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">Honors</Link></Typography>
+                <Typography variant="subtitle2" color="grey.400">
+                  Company Profile
+                </Typography>
+                <Typography variant="body2">
+                  <Link href="/home" color="inherit"    underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}>
+                    Overview
+                  </Link>
+                </Typography>
+                <Typography variant="body2">
+                  <Link href="/aboutus" color="inherit"    underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}>
+                    History
+                  </Link>
+                </Typography>
+              
               </Grid>
               <Grid item xs={6} sm={3}>
-                <Typography variant="subtitle2" color="grey.400">Products</Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">Wiring Harness</Link></Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">Power Plug</Link></Typography>
+                <Typography variant="subtitle2" color="grey.400">
+                  Products
+                </Typography>
+                <Typography variant="body2">
+                  <Link href="/products" color="inherit"    underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}>
+                    Wiring Harness
+                  </Link>
+                </Typography>
+           
               </Grid>
               <Grid item xs={6} sm={3}>
-                <Typography variant="subtitle2" color="grey.400">Applications</Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">Vehicle</Link></Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">Non-road solutions</Link></Typography>
+                <Typography variant="subtitle2" color="grey.400">
+                  Applications
+                </Typography>
+                <Typography variant="body2">
+                  <Link href="/application" color="inherit"    underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}>
+                    Vehicle
+                  </Link>
+                </Typography>
+             
               </Grid>
               <Grid item xs={6} sm={3}>
-                <Typography variant="subtitle2" color="grey.400">Support</Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">Technical Support</Link></Typography>
-                <Typography variant="body2"><Link href="#" color="inherit">Brand Info</Link></Typography>
+                <Typography variant="subtitle2" color="grey.400">
+                  Support
+                </Typography>
+             
+                <Typography variant="body2">
+                  <Link href="#" color="inherit"    underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}>
+                    Brand Info
+                  </Link>
+                   </Typography>
+                                   <Typography variant="body2">
+                    <Link href="/electrical" color="inherit"    underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}>
+                    Electrical
+                  </Link>
+                    </Typography>
+                     <Typography variant="body2">
+                    <Link href="/electronics" color="inherit"    underline="none"
+                sx={{
+                  "&:hover": {
+                    color: "#fff",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  },
+                }}>
+                    Electronics
+                  </Link>
+                </Typography>
               </Grid>
             </Grid>
           </Grid>
